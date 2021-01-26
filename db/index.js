098c5a29b6151174b72683afde32d8fdc87724e0
Greg <andyb4rd@gmail.com>
2021-01-26 14:00:37 -0500

    Database Finished

---
+++ db/index.js
@@ -0,0 +1,245 @@
+// Connect to DB
+const { Client } = require("pg");
+const DB_NAME = "gregorator-dev";
+const DB_URL = process.env.DATABASE_URL || {
+  user: "postgres",
+  host: "localhost",
+  database: "gregorator-dev",
+  password: "2112",
+  port: 5432,
+};
+const client = new Client(DB_URL);
+
+// database methods
+
+async function createLink({ comment, date, url, tags = [] }) {
+  try {
+    const {
+      rows: [link],
+    } = await client.query(
+      `
+      INSERT INTO links(url, comment, date, clickCount) 
+      VALUES($1, $2, $3, $4) 
+      ON CONFLICT (url) DO NOTHING 
+      RETURNING *;
+      `,
+      [url, comment, date, "0"]
+    );
+
+    const tagList = await createTags(tags);
+
+    return await addTagsToLink(link.id, tagList);
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createTags(tagList) {
+  if (tagList.length === 0) {
+    return;
+  }
+
+  const valuesStringInsert = tagList
+    .map((_, index) => `$${index + 1}`)
+    .join("), (");
+
+  const valuesStringSelect = tagList
+    .map((_, index) => `$${index + 1}`)
+    .join(", ");
+
+  try {
+    // insert all, ignoring duplicates
+    await client.query(
+      `
+      INSERT INTO tags(name)
+      VALUES (${valuesStringInsert})
+      ON CONFLICT (name) DO NOTHING;
+    `,
+      tagList
+    );
+
+    // grab all and return
+    const { rows: tags } = await client.query(
+      `
+      SELECT * FROM tags
+      WHERE name
+      IN (${valuesStringSelect});
+    `,
+      tagList
+    );
+
+    return tags;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createLinkTag(linkId, tagId) {
+  try {
+    const {
+      rows: [tag],
+    } = await client.query(
+      `
+      INSERT INTO link_tags("linkId", "tagId")
+      VALUES ($1, $2)
+      ON CONFLICT ("linkId", "tagId") DO NOTHING;
+    `,
+      [linkId, tagId]
+    );
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function addTagsToLink(linkId, tagList) {
+  try {
+    const createLinkTagPromises = tagList.map((tag) =>
+      createLinkTag(linkId, tag.id)
+    );
+
+    await Promise.all(createLinkTagPromises);
+
+    return await getLinkById(linkId);
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getLinkById(linkId) {
+  try {
+    const {
+      rows: [link],
+    } = await client.query(
+      `
+      SELECT *
+      FROM links
+      WHERE id=$1;
+    `,
+      [linkId]
+    );
+
+    if (!link) {
+      throw {
+        name: "LinkNotFoundError",
+        message: "Could not find a link with that linkId",
+      };
+    }
+
+    const { rows: tags } = await client.query(
+      `
+      SELECT tags.*
+      FROM tags
+      JOIN link_tags ON tags.id=link_tags."tagId"
+      WHERE link_tags."linkId"=$1;
+    `,
+      [linkId]
+    );
+
+    link.tags = tags;
+
+    return link;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getAllLinks() {
+  try {
+    const { rows: linkIds } = await client.query(`
+SELECT id
+FROM links;
+`);
+    const links = await Promise.all(
+      linkIds.map(async (link) => {
+        const linkObj = getLinkById(link.id);
+        return linkObj;
+      })
+    );
+    return links;
+  } catch (error) {
+    console.log("error running getAllLinks");
+    throw error;
+  }
+}
+
+async function getLinksByTagName(tagName) {
+  try {
+    const { rows: linkIds } = await client.query(
+      `
+      SELECT links.id
+      FROM links
+      JOIN link_tags ON links.id=link_tags."linkId"
+      JOIN tags ON tags.id=link_tags."tagId"
+      WHERE LOWER(tags.name)=LOWER($1);
+    `,
+      [tagName]
+    );
+
+    return await Promise.all(linkIds.map((link) => getLinkById(link.id)));
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateLink(linkId, fields = {}) {
+  // read off the tags & remove that field
+  const { tags } = fields; // might be undefined
+  delete fields.tags;
+
+  // build the set string
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+
+  try {
+    // update any fields that need to be updated
+    if (setString.length > 0) {
+      await client.query(
+        `
+        UPDATE links
+        SET ${setString}
+        WHERE id=${linkId}
+        RETURNING *;
+      `,
+        Object.values(fields)
+      );
+    }
+
+    // return early if there's no tags to update
+    if (tags === undefined) {
+      return await getLinkById(linkId);
+    }
+
+    // make any new tags that need to be made
+    const tagList = await createTags(tags);
+    const tagListIdString = tagList.map((tag) => `${tag.id}`).join(", ");
+
+    // delete any link_tags from the database which aren't in that tagList
+    await client.query(
+      `
+      DELETE FROM link_tags
+      WHERE "tagId"
+      NOT IN (${tagListIdString})
+      AND "linkId"=$1;
+    `,
+      [linkId]
+    );
+
+    // and create link_tags as necessary
+    await addTagsToLink(linkId, tagList);
+
+    return await getLinkById(linkId);
+  } catch (error) {
+    throw error;
+  }
+}
+
+// export
+module.exports = {
+  client,
+  // db methods
+  createLink,
+  getAllLinks,
+  getLinksByTagName,
+  updateLink,
+};
---
+++ db/init_db.js
@@ -0,0 +1,113 @@
+// code to build and initialize DB goes here
+const {
+  client,
+  createLink,
+  getAllLinks,
+  getLinksByTagName,
+  updateLink,
+  // other db methods
+} = require("./index");
+
+async function buildTables() {
+  try {
+    client.connect();
+
+    // drop tables in correct order
+    console.log("Starting to drop tables...");
+    await client.query(`
+    DROP TABLE IF EXISTS link_tags;
+    DROP TABLE IF EXISTS tags;
+    DROP TABLE IF EXISTS links;
+  `);
+    console.log("Finished dropping tables!");
+    // build tables in correct order
+    console.log("Starting to build tables...");
+
+    await client.query(`
+    CREATE TABLE links (
+      id SERIAL PRIMARY KEY,
+      url varchar(255) UNIQUE NOT NULL,
+      comment varchar(255) NOT NULL,
+      date varchar(255) NOT NULL,
+      clickCount varchar(255)
+    );
+
+    CREATE TABLE tags (
+      id SERIAL PRIMARY KEY,
+      name varchar(255) UNIQUE NOT NULL
+    );
+
+    CREATE TABLE link_tags (
+      "linkId" INTEGER REFERENCES links(id),
+      "tagId" INTEGER REFERENCES tags(id),
+      UNIQUE ("linkId", "tagId")
+    );
+  `);
+    console.log("Finished building tables!");
+  } catch (error) {
+    console.error("error running buildTables");
+    throw error;
+  }
+}
+
+async function populateInitialData() {
+  try {
+    // create useful starting data
+    console.log("starting to create links...");
+    await createLink({
+      comment: "Bill Cosby is on Netflix!",
+      date: "1/25/2021",
+      url: "https://www.youtube.com/watch?v=oGvURV9fngM",
+      tags: ["funny", "informative", "cosby"],
+    });
+    await createLink({
+      comment: "Who's he peepin' at though?",
+      date: "1/26/2021",
+      url: "https://www.youtube.com/watch?v=MC2XkigeXiI",
+      tags: ["funny", "walking", "cosby"],
+    });
+    await createLink({
+      comment: "Video I made for school in like 9th grade",
+      date: "1/26/2021",
+      url: "https://www.youtube.com/watch?v=oAulKa4C4-Q",
+      tags: ["ancient", "walking", "reflective"],
+    });
+    console.log("finished creating test links!");
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function testDb() {
+  try {
+    console.log("Starting to test database...");
+
+    console.log("Calling getAllLinks");
+    const links = await getAllLinks();
+    console.log("Result: ", links);
+
+    console.log("Calling getLinksByTagName with 'Cosby'...");
+    const linksByTag = await getLinksByTagName("Cosby");
+    console.log("Result: ", linksByTag);
+
+    console.log("Calling updateLink on links[0]");
+    const updateLinkResult = await updateLink(links[0].id, {
+      url: "https://www.youtube.com/watch?v=h4fRnJjhZ64&t=23s",
+      comment: "SLIIIINKYYYY",
+      tags: ["slinky", "funny", "toystory"],
+      date: "1/26/21",
+    });
+    console.log("Result:", updateLinkResult);
+
+    console.log("Finished testing database.");
+  } catch (error) {
+    console.log("error testing database");
+    throw error;
+  }
+}
+
+buildTables()
+  .then(populateInitialData)
+  .then(testDb)
+  .catch(console.error)
+  .finally(() => client.end());
